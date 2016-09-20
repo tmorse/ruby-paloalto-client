91818f12e3c9bc81a9bee34a6b22a97bd3b67b3e
Tom Morse <thomasmorse@me.com>
2016-09-20 09:32:43 -0400

    add net_policy api shell

---
+++ lib/palo_alto/v6/nat_policy_api.rb
@@ -0,0 +1,20 @@
+require "palo_alto/models/device"
+require "palo_alto/models/virtual_system"
+
+module PaloAlto
+  module V6
+    module NatPolicyAPI
+      # Parse out the nat_polices from a response to query for nat_policies
+      #
+      # == Returns
+      #
+      #  * +Array+ - Array of Models::Nat_Policy instances
+      #
+      # == Raises
+      #
+      #  * +Exception+ - Raises an exception if the request is unsuccessful
+      def nat_policies
+      end
+    end
+  end
+end
